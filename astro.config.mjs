d94884763511ed25ee37a3c724496313e6cea742
JustMarkDev <procopiomarco@protonmail.com>
2026-05-07 11:50:46 +0200

    Revert "fix: disable CSRF origin check to allow form submissions on Vercel"
    This reverts commit 67726a38c3d1a2e7d72cd7e58c15f89bb0b69e1a.

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,9 +10,6 @@ import vercel from "@astrojs/vercel";
 export default defineConfig({
   site: "https://sergioprocopio.com",
   output: "static",
-  security: {
-    checkOrigin: false,
-  },
   adapter: vercel({
     webAnalytics: { enabled: true },
   }),
